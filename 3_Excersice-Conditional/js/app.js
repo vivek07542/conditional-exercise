68a9c83495615fb62aa2f225c7749767e5aab9cc
unknown <vivekbindal1209@gmail.com>
2020-08-29 23:35:34 +0530

    Conditional Exercise-Party Entry Console

---
+++ 3_Excersice-Conditional/js/app.js
@@ -0,0 +1,24 @@
+//Var Age Factor For bonus 
+var age= Number(prompt("What is Your age"));
+
+// Var If Age is Negative
+if (age < 0) {
+  console.log("Come Back Once you are out of womb");
+}
+//Var Else If Age Is Equal to 21
+if (age === 21){
+  console.log("Happy 21 Birthday");
+}
+// Var If Age is ODD
+if (age % 2 !==0){
+  console.log(You age is Odd );
+}
+// If age is a perfect square
+if(age % Math.sqrt(age) === 0) {
+  console.log("Your age is a perfect square!");
+}
+var num= 1;
+while(num <=10);{
+  console.log(num);
+  num+=2;
+}
